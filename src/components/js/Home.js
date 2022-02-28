c75d72aa2b4d92f841ad5c2469f22688eefafd60
Indersingh987 <Indersingh987.is@gmail.com>
2022-02-28 15:43:22 +0530

    header UI

--- src/components/js/Home.js
+++ src/components/js/Home.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { useNavigate } from "react-router-dom";
+import Header from "./Header"
+import '../css/Home.css'
 
 export default function Home() {
-  const navigate = useNavigate();
-  function goToLogin(){
-    navigate('/login');
-  }
+  
   return (
-    <div>
-        <button onClick={goToLogin}>sign in</button>
+    <div className= "home">
+        <Header />
+        
     </div>
   )
 }
