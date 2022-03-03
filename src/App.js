bc903f0dc803d116a14a52d0d0c05777acf61eef
Arshdeep Singh <arsh76278@gmail.com>
2022-03-03 13:46:32 +0530

    Updated routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import './App.css';
-import {BrowserRouter as Router ,Route,Routes} from "react-router-dom"
+import {BrowserRouter as Router ,Route,Routes,Outlet} from "react-router-dom"
 import Home from './components/js/Home';
 import Login from './components/js/Login';
 import Register from './components/js/Register';
+import Product from './components/js/Product';
+import Products from './components/js/Products';
  
 function App() {
   return (
     <Router>
       <div className="app">
         <Routes>
-          <Route path={'/'} exact element={<Home />} />
-          <Route path={'/login'} exact element={<Login />} />
-          <Route path={'/register'} exact element={<Register />} />
+          <Route path='/'  element={<Home />}>
+             <Route path='/' element={<Products/>} />
+             <Route path='/product' element={<Product />} />
+          </Route>
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
         </Routes>
-          
+        <Outlet />
       </div>
     </Router>
     
